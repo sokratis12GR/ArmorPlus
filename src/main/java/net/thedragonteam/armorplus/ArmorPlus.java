dc4806fbc6b885b1068b97a99010ef11a2a69795
sokratis12GR <sokratis12GR@gmail.com>
2016-08-08 10:29:09 +0300

    fixed ArmorForge's GUI not opeing

--- src/main/java/net/thedragonteam/armorplus/ArmorPlus.java
+++ src/main/java/net/thedragonteam/armorplus/ArmorPlus.java
@@ -53,7 +53,7 @@ import static net.thedragonteam.armorplus.client.gui.GuiHandler.GUI_ARMOR_FORGE;
 public class ArmorPlus {
 
     public static final String MODID = "armorplus";
-    public static final String VERSION = "1.10.2-5.0.5.0";
+    public static final String VERSION = "1.10.2-5.0.5.1";
     public static final String MODNAME = "ArmorPlus";
     public static final String DEPEND = "required-after:thedragoncore@[" + TheDragonCore.VERSION + ",);";
     public static final String CLIENTPROXY = "net.thedragonteam.armorplus.proxy.ClientProxy";
@@ -113,6 +113,7 @@ public class ArmorPlus {
         logger.info(TextHelper.localize("info." + ArmorPlus.MODID + ".console.load.init"));
         logger.info("Version " + ArmorPlus.VERSION + " initializing...");
         MinecraftForge.EVENT_BUS.register(new GlobalEventsArmorPlus());
+        NetworkRegistry.INSTANCE.registerGuiHandler(this, GuiHandler);
 
         //Register to receive subscribed events
         MinecraftForge.EVENT_BUS.register(this);
