b50b5651888db17779b38dd7189b99fa202f80d0
sokratis12GR <sokratis12GR@gmail.com>
2016-09-10 15:42:17 +0300

    Fixed EnderDragonZombie's Drop Rates

--- src/main/java/net/thedragonteam/armorplus/ArmorPlus.java
+++ src/main/java/net/thedragonteam/armorplus/ArmorPlus.java
@@ -61,9 +61,9 @@ public class ArmorPlus {
     // Updates every time the API change, resets on MAJOR changes
     public static final int API = 0;
     // Updates every time a new block, item or features is added or change, resets on MAJOR changes
-    public static final int MINOR = 10;
+    public static final int MINOR = 9;
     // Updates every time a new block, item or features is added or change, resets on MINOR changes
-    public static final int PATCH = 0;
+    public static final int PATCH = 2;
     // The ArmorPlus Version
     public static final String VERSION =
             ArmorPlus.MCVERSION + "-" + ArmorPlus.MAJOR + "." + ArmorPlus.API + "." + ArmorPlus.MINOR + "." + ArmorPlus.PATCH;
