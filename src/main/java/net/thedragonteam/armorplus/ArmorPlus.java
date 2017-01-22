4c3b6f9f9c9469f1c49abb8e9da2a769d5878517
Sokratis Fotkatzikis <sokratis12GR@gmail.com>
2017-01-23 00:56:09 +0200

    Updated Forge, Mappings, Dependencies and Mod versions

--- src/main/java/net/thedragonteam/armorplus/ArmorPlus.java
+++ src/main/java/net/thedragonteam/armorplus/ArmorPlus.java
@@ -40,18 +40,18 @@ public class ArmorPlus {
      * Updates every time a new block, item or features is added or change,
      * resets on MAJOR changes
      */
-    public static final int MINOR = 4;
+    public static final int MINOR = 5;
     /**
      * Updates every time a bug is fixed or issue solved or very minor code changes,
      * resets on MINOR changes
      */
-    public static final int PATCH = 1;
+    public static final int PATCH = 2;
     /**
      * Updates every time a build is created, mostly used for dev versions and
      * final versions for releases after for each Minor or Major update,
      * resets on MINOR and MAJOR changes
      */
-    public static final int BUILD = 2;
+    public static final int BUILD = 3;
     /**
      * The ArmorPlus Version
      */
