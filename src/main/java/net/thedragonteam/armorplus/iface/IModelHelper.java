150215b3bf4fa67aa12a8a7ec9e3b21147974dbe
Sokratis Fotkatzikis <sokratis12GR@gmail.com>
2017-02-16 23:57:00 +0200

    Improved the interface

--- src/main/java/net/thedragonteam/armorplus/iface/IModelHelper.java
+++ src/main/java/net/thedragonteam/armorplus/iface/IModelHelper.java
@@ -12,6 +12,8 @@ import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import javax.annotation.Nonnull;
+
 public interface IModelHelper {
 
     @SideOnly(Side.CLIENT)
@@ -20,8 +22,18 @@ public interface IModelHelper {
     }
 
     @SideOnly(Side.CLIENT)
+    default void initModel(Block block, ResourceLocation registryName, int meta, String variantIn) {
+        initModel(Item.getItemFromBlock(block), registryName.toString(), meta, variantIn);
+    }
+
+    @SideOnly(Side.CLIENT)
     default void initModel(Block block, String registryName, int meta) {
         initModel(Item.getItemFromBlock(block), registryName, meta);
+    }
+
+    @SideOnly(Side.CLIENT)
+    default void initModel(Block block, String registryName, int meta, String variantIn) {
+        initModel(Item.getItemFromBlock(block), registryName, meta, variantIn);
     }
 
     @SideOnly(Side.CLIENT)
@@ -30,7 +42,17 @@ public interface IModelHelper {
     }
 
     @SideOnly(Side.CLIENT)
+    default void initModel(Item item, ResourceLocation registryName, int meta, String variantIn) {
+        initModel(item, registryName.toString(), meta, variantIn);
+    }
+
+    @SideOnly(Side.CLIENT)
     default void initModel(Item item, String registryName, int meta) {
         ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(registryName, "inventory"));
+    }
+
+    @SideOnly(Side.CLIENT)
+    default void initModel(Item item, String registryName, int meta, @Nonnull String variantIn) {
+        ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(registryName, variantIn));
     }
 }
