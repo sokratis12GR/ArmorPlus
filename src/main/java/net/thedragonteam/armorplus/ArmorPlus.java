bc941f339ee9580952b2f10fcd9e828b1be2ed6a
Sokratis Fotkatzikis <sokratis12GR@gmail.com>
2017-02-18 14:17:27 +0200

    Updqted Version

--- src/main/java/net/thedragonteam/armorplus/ArmorPlus.java
+++ src/main/java/net/thedragonteam/armorplus/ArmorPlus.java
@@ -41,18 +41,18 @@ public class ArmorPlus {
      * Updates every time a new block, item or features is added or change,
      * resets on MAJOR changes
      */
-    public static final int MINOR = 5;
+    public static final int MINOR = 6;
     /**
      * Updates every time a bug is fixed or issue solved or very minor code changes,
      * resets on MINOR changes
      */
-    public static final int PATCH = 7;
+    public static final int PATCH = 0;
     /**
      * Updates every time a build is created, mostly used for dev versions and
      * final versions for releases after for each Minor or Major update,
      * resets on MINOR and MAJOR changes
      */
-    public static final int BUILD = 8;
+    public static final int BUILD = 1;
     /**
      * The ArmorPlus Version
      */
