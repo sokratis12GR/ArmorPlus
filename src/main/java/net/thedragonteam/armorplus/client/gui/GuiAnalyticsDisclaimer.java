85ab09c797c3c2cac49e16b15923aac1ee267327
Eric Schneider <xbony2@users.noreply.github.com>
2017-06-24 09:33:35 -0400

    fixed spelling

--- src/main/java/net/thedragonteam/armorplus/client/gui/GuiAnalyticsDisclaimer.java
+++ src/main/java/net/thedragonteam/armorplus/client/gui/GuiAnalyticsDisclaimer.java
@@ -18,7 +18,7 @@ public class GuiAnalyticsDisclaimer extends GuiScreen {
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         this.drawDefaultBackground();
         int offset = Math.max(85, 10);
-        this.drawCenteredString(this.fontRenderer, "ArmorPlus uses annonymous analytics to improve our mod", this.width / 2, offset, 0xFFFFFF);
+        this.drawCenteredString(this.fontRenderer, "ArmorPlus uses anonymous analytics to improve our mod", this.width / 2, offset, 0xFFFFFF);
         this.drawCenteredString(this.fontRenderer, "Do you want to Opt-In:", this.width / 2, offset + 25, 0xFFFFFF);
         this.buttonList.add(new GuiOptionButton(0, this.width / 2 - 155, this.height / 6 + 96, "Yes"));
         this.buttonList.add(new GuiOptionButton(1, this.width / 2 - 155 + 160, this.height / 6 + 96, "No"));
