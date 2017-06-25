007275c04fdd97b82f721f5fe87d521fb10adeaa
Sokratis Fotkatzikis <sokratis12GR@gmail.com>
2017-06-25 11:18:01 +0300

    Reworked the IModelHelper Interface

--- src/main/java/net/thedragonteam/armorplus/iface/IModelHelper.java
+++ src/main/java/net/thedragonteam/armorplus/iface/IModelHelper.java
@@ -13,49 +13,62 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.Objects;
 
+import static java.lang.String.format;
 import static net.minecraftforge.client.model.ModelLoader.setCustomModelResourceLocation;
+import static net.thedragonteam.armorplus.ArmorPlus.MODID;
 import static net.thedragonteam.thedragonlib.util.ItemStackUtils.getItem;
 
 public interface IModelHelper {
 
     @SideOnly(Side.CLIENT)
-    default void initModel(Object object, String registryName, int meta) {
-        if (object instanceof Block) {
-            setCustomModelResourceLocation(getItem((Block) object), meta, new ModelResourceLocation(registryName, "inventory"));
-        } else if (object instanceof Item) {
-            setCustomModelResourceLocation((Item) object, meta, new ModelResourceLocation(registryName, "inventory"));
-        }
-    }
-
-    @SideOnly(Side.CLIENT)
-    default void initModel(Object object, String registryName, int meta, String variantIn) {
+    default void initModel(ResourceLocation registryName, String suffix, String location, int meta, String variantIn) {
+        String resourcePath = (location != null && !Objects.equals(location, ""))
+                ? (location + "/" + registryName.getResourcePath() + suffix)
+                : registryName.getResourcePath() + suffix;
+        String resourceLocation = format("%s:%s", MODID, resourcePath);
         if (variantIn == null || Objects.equals(variantIn, "")) variantIn = "inventory";
-        if (object instanceof Block) {
-            setCustomModelResourceLocation(getItem((Block) object), meta, new ModelResourceLocation(registryName, variantIn));
-        } else if (object instanceof Item) {
-            setCustomModelResourceLocation((Item) object, meta, new ModelResourceLocation(registryName, variantIn));
+        ModelResourceLocation mrl = new ModelResourceLocation(resourceLocation, variantIn);
+        if (this instanceof Block) {
+            setCustomModelResourceLocation(getItem((Block) this), meta, mrl);
+        } else if (this instanceof Item) {
+            setCustomModelResourceLocation((Item) this, meta, mrl);
         }
     }
 
     @SideOnly(Side.CLIENT)
-    default void initModel(Object object, ResourceLocation registryName, int meta) {
-        if (object instanceof Block) {
-            setCustomModelResourceLocation(getItem((Block) object), meta, new ModelResourceLocation(registryName, "inventory"));
-        } else if (object instanceof Item) {
-            setCustomModelResourceLocation((Item) object, meta, new ModelResourceLocation(registryName, "inventory"));
-        }
+    default void initModel(ResourceLocation registryName, String suffix, String location, int meta) {
+        this.initModel(registryName, suffix, location, meta, "");
     }
 
     @SideOnly(Side.CLIENT)
-    default void initModel(Object object, ResourceLocation registryName, int meta, String variantIn) {
-        if (variantIn == null || Objects.equals(variantIn, "")) variantIn = "inventory";
-        if (object instanceof Block) {
-            setCustomModelResourceLocation(getItem((Block) object), meta, new ModelResourceLocation(registryName, variantIn));
-        } else if (object instanceof Item) {
-            setCustomModelResourceLocation((Item) object, meta, new ModelResourceLocation(registryName, variantIn));
-        }
+    default void initModel(ResourceLocation registryName, String suffix, String location) {
+        this.initModel(registryName, suffix, location, 0);
     }
 
     @SideOnly(Side.CLIENT)
+    default void initModel(ResourceLocation registryName, String location, int meta, String variantIn) {
+        this.initModel(registryName, "", location, meta, variantIn);
+    }
+
+    @SideOnly(Side.CLIENT)
+    default void initModel(ResourceLocation registryName, String location, int meta) {
+        this.initModel(registryName, location, meta, "");
+    }
+
+    @SideOnly(Side.CLIENT)
+    default void initModel(ResourceLocation registryName, int meta, String variantIn) {
+        this.initModel(registryName, "", meta, variantIn);
+    }
+
+    @SideOnly(Side.CLIENT)
+    default void initModel(ResourceLocation registryName, String location) {
+        this.initModel(registryName, location, 0);
+    }
+
+    @SideOnly(Side.CLIENT)
+    default void initModel(ResourceLocation registryName, int meta) {
+        this.initModel(registryName, meta, "");
+    }
+
     void initModel();
 }
