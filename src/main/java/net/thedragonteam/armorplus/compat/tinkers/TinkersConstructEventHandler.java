e884fe307c0db4b312a22c973dc63de1619d17f7
Sokratis Fotkatzikis <sokratis12GR@gmail.com>
2017-12-21 00:46:55 +0200

    re-added tinkers' integration

--- src/main/java/net/thedragonteam/armorplus/compat/tinkers/TinkersConstructEventHandler.java
+++ src/main/java/net/thedragonteam/armorplus/compat/tinkers/TinkersConstructEventHandler.java
@@ -1,14 +1,19 @@
 package net.thedragonteam.armorplus.compat.tinkers;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.thedragonteam.armorplus.compat.tinkers.modifiers.TiCModifiers;
+import slimeknights.tconstruct.library.events.TinkerRegisterEvent;
+
 /**
  * @author Sokratis Fotkatzikis - TheDragonTeam
  */
-//@Mod.EventBusSubscriber(modid = "tconstruct")
-//public class TinkersConstructEventHandler {
-//
-//    @SubscribeEvent
-//    public static void onModifierRegister(TinkerRegisterEvent.ModifierRegisterEvent event) {
-//        new TinkerRegisterEvent.ModifierRegisterEvent(TiCModifiers.theUltimateMaterial);
-//        new TinkerRegisterEvent.ModifierRegisterEvent(TiCModifiers.infusedLavaCrystalModifier);
-//    }
-//}
+@Mod.EventBusSubscriber(modid = "tconstruct")
+public class TinkersConstructEventHandler {
+
+    @SubscribeEvent
+    public static void onModifierRegister(TinkerRegisterEvent.ModifierRegisterEvent event) {
+        new TinkerRegisterEvent.ModifierRegisterEvent(TiCModifiers.theUltimateMaterial);
+        new TinkerRegisterEvent.ModifierRegisterEvent(TiCModifiers.infusedLavaCrystalModifier);
+    }
+}
--- src/main/java/net/thedragonteam/armorplus/compat/tinkers/modifiers/TiCModifiers.java
+++ src/main/java/net/thedragonteam/armorplus/compat/tinkers/modifiers/TiCModifiers.java
@@ -4,20 +4,29 @@
 
 package net.thedragonteam.armorplus.compat.tinkers.modifiers;
 
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.thedragonteam.armorplus.registry.APItems;
+import net.thedragonteam.armorplus.registry.ModItems;
+import slimeknights.mantle.util.RecipeMatch;
+import slimeknights.tconstruct.library.modifiers.Modifier;
+import slimeknights.tconstruct.tools.AbstractToolPulse;
+
+import static net.thedragonteam.thedragonlib.util.ItemStackUtils.getItemStack;
+
 /**
  * @author Sokratis Fotkatzikis - TheDragonTeam
  */
-//public class TiCModifiers extends AbstractToolPulse {
-//
-//    public static Modifier theUltimateMaterial;
-//    public static Modifier infusedLavaCrystalModifier;
-//
-//    private static final ItemStack infusedLavaCrystal = getItemStack(ModItems.lavaCrystal, 1);
-//
-//    public void init(FMLInitializationEvent event) {
-//        theUltimateMaterial = registerModifier(new ModExtraModifier());
-//        infusedLavaCrystalModifier = registerModifier(new ModFireStorm());
-//        theUltimateMaterial.addItem(APItems.theUltimateMaterial, 3, 1);
-//        infusedLavaCrystalModifier.addRecipeMatch(new RecipeMatch.ItemCombination(2, infusedLavaCrystal));
-//    }
-//}
+public class TiCModifiers extends AbstractToolPulse {
+
+    private static final ItemStack infusedLavaCrystal = getItemStack(ModItems.lavaCrystal, 1);
+    public static Modifier theUltimateMaterial;
+    public static Modifier infusedLavaCrystalModifier;
+
+    public void init(FMLInitializationEvent event) {
+        theUltimateMaterial = registerModifier(new ModExtraModifier());
+        infusedLavaCrystalModifier = registerModifier(new ModFireStorm());
+        theUltimateMaterial.addItem(APItems.theUltimateMaterial, 3, 1);
+        infusedLavaCrystalModifier.addRecipeMatch(new RecipeMatch.ItemCombination(2, infusedLavaCrystal));
+    }
+}
