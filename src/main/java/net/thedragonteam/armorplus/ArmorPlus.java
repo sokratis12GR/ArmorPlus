b350a527cf089a14eb10c39179e98e7ea6509c7e
Sokratis Fotkatzikis <sokratis12GR@gmail.com>
2017-12-21 00:39:02 +0200

    updated mod version

--- src/main/java/net/thedragonteam/armorplus/ArmorPlus.java
+++ src/main/java/net/thedragonteam/armorplus/ArmorPlus.java
@@ -54,13 +54,13 @@ public class ArmorPlus {
      * Updates every time a bug is fixed or issue solved or very minor code changes,
      * resets on MINOR changes
      */
-    public static final int PATCH = 0;
+    public static final int PATCH = 3;
     /**
      * Updates every time a build is created, mostly used for dev versions and
      * final versions for releases after for each Minor or Major update,
      * resets on MAJOR changes
      */
-    public static final int BUILD = 26;
+    public static final int BUILD = 29;
     /**
      * The ArmorPlus Version
      */
