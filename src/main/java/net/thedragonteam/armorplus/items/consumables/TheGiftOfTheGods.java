85c22a21f51f6a5b40861910c3e288dea468825b
Sokratis Fotkatzikis <sokratis12GR@gmail.com>
2017-12-29 18:33:12 +0200

    Fixed TGOTG's tooltip information

--- src/main/java/net/thedragonteam/armorplus/items/consumables/TheGiftOfTheGods.java
+++ src/main/java/net/thedragonteam/armorplus/items/consumables/TheGiftOfTheGods.java
@@ -24,12 +24,12 @@ import net.thedragonteam.thedragonlib.util.ItemStackUtils;
 import net.thedragonteam.thedragonlib.util.LogHelper;
 
 import javax.annotation.Nullable;
-import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-import static net.minecraft.util.text.TextFormatting.GOLD;
+import static java.text.MessageFormat.format;
+import static net.minecraft.util.text.TextFormatting.*;
 import static net.thedragonteam.armorplus.APConfig.*;
 import static net.thedragonteam.armorplus.util.EnumHelperUtil.addRarity;
 import static net.thedragonteam.armorplus.util.TextUtils.setTextTranslation;
@@ -50,13 +50,12 @@ public class TheGiftOfTheGods extends BaseItem {
         this.setCreativeTab(ArmorPlus.tabArmorplusItems);
     }
 
-
     @Override
     public EnumRarity getRarity(ItemStack stack) {
         return golden;
     }
 
-
+    @SuppressWarnings("unchecked")
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand hand) {
         String[] blackListedItems = APConfig.blackListedItems;
@@ -103,7 +102,7 @@ public class TheGiftOfTheGods extends BaseItem {
                 playerIn.dropItem(item, 1);
                 playerIn.sendStatusMessage(setTextTranslation("status.armorplus.tgotg.gained_item", item.getItemStackDisplayName(playerIn.getHeldItem(hand)), item.getRegistryName()), false);
                 if (debugMode && debugModeTGOTG)
-                    LogHelper.info(MessageFormat.format("Item''s Registry Name: {0};\n Item''s Creative Tab: {1}\n{2}{3}", item.getRegistryName(), item.getCreativeTab(),
+                    LogHelper.info(format("Item''s Registry Name: {0};\n Item''s Creative Tab: {1}\n{2}{3}", item.getRegistryName(), item.getCreativeTab(),
                         " ; Item's Unlocalized Name: " + item.getUnlocalizedName() + ";\n Does the Item have Subtypes: " + item.getHasSubtypes() + "\n",
                         " ; Item's Max Damage: " + getMaxDamage(getItemStack(item))));
             }
@@ -117,7 +116,7 @@ public class TheGiftOfTheGods extends BaseItem {
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
         KeyBinding keyBindSneak = Minecraft.getMinecraft().gameSettings.keyBindSneak;
         int maxUses = APConfig.maxUses;
-        tooltip.add("$ITALIC${RED}This item can summon items which can potentially cause crashes");
+        tooltip.add(format("{0}{1}This item can summon items which can potentially cause crashes", ITALIC, RED));
         if (GameSettings.isKeyDown(keyBindSneak)) {
             tooltip.add("\u00a79Ability: \u00a7rGrants Random Item");
             tooltip.add("\u00a79Max Uses: \u00a7r" + maxUses);
