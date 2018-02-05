b71bfb59a2e9384907ccc83af10fa92bce8a35ac
Sokratis Fotkatzikis <sokratis12GR@gmail.com>
2018-02-05 13:31:19 +0200

    Fixed default methods in IEffectHolder

--- src/main/java/net/thedragonteam/armorplus/api/properties/IEffectHolder.java
+++ src/main/java/net/thedragonteam/armorplus/api/properties/IEffectHolder.java
@@ -1,19 +1,19 @@
 package net.thedragonteam.armorplus.api.properties;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public interface IEffectHolder {
 
     default List<String> getApplyEffectNames() {
-        return Collections.emptyList();
+        return new ArrayList<>();
     }
 
     default List<Integer> getApplyAmplifierLevels() {
-        return Collections.emptyList();
+        return new ArrayList<>();
     }
 
     default List<String> getRemoveEffectNames() {
-        return Collections.emptyList();
+        return new ArrayList<>();
     }
 }
