1c2b4e1b1986446fa0cca61fdf8220ea7859c0be
Sokratis Fotkatzikis <sokratis.fotkatzikis@staclar.com>
2020-06-01 10:22:18 +0300

    Registered all the registries
    So that they actually register

--- src/main/java/com/sofodev/armorplus/ArmorPlus.java
+++ src/main/java/com/sofodev/armorplus/ArmorPlus.java
@@ -1,22 +1,37 @@
 package com.sofodev.armorplus;
 
 import com.sofodev.armorplus.registry.APItems;
-import com.sofodev.armorplus.registry.ModItems;
+import com.sofodev.armorplus.registry.blocks.castle.BrickColor;
+import com.sofodev.armorplus.registry.blocks.special.SwordDisplayTile;
+import com.sofodev.armorplus.registry.blocks.special.TESRSwordDisplay;
+import com.sofodev.armorplus.registry.entities.arrows.APArrowEntity;
+import com.sofodev.armorplus.registry.entities.arrows.APArrowRenderer;
+import com.sofodev.armorplus.registry.entities.arrows.impl.*;
 import com.sofodev.armorplus.registry.items.armors.APArmorMaterial;
 import com.sofodev.armorplus.registry.items.tools.properties.APToolProperties;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.potion.Effect;
+import net.minecraft.tileentity.TileEntityType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import static com.sofodev.armorplus.utils.Utils.setName;
+import static com.sofodev.armorplus.utils.Utils.setRL;
 import static net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.MOD;
 
 @Mod("armorplus")
@@ -31,17 +46,22 @@ public class ArmorPlus {
 
     public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, MODID);
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, MODID);
+    public static final DeferredRegister<Enchantment> ENCHANTMENTS = new DeferredRegister<>(ForgeRegistries.ENCHANTMENTS, MODID);
+    public static final DeferredRegister<TileEntityType<?>> TILE_ENTITIES = new DeferredRegister<>(ForgeRegistries.TILE_ENTITIES, MODID);
+    public static final DeferredRegister<EntityType<?>> ENTITIES = new DeferredRegister<>(ForgeRegistries.ENTITIES, MODID);
+    public static final DeferredRegister<Effect> EFFECTS = new DeferredRegister<>(ForgeRegistries.POTIONS, MODID);
 
     /**
      * Used as an "upper ground" variable, which sets the limit for the sets which use these materials.
      */
     public static final int AP_ARMOR_MATERIAL_LENGTH = APArmorMaterial.values().length;
     public static final int AP_TOOL_MATERIAL_LENGTH = APToolProperties.values().length;
+    public static final int AP_STONE_BRICKS_LENGTH = BrickColor.values().length;
 
     public static final ItemGroup AP_GROUP = new ItemGroup(ItemGroup.getGroupCountSafe(), setName("armors")) {
         @Override
         public ItemStack createIcon() {
-            return new ItemStack(ModItems.CHESTPLATES[7].get());
+            return new ItemStack(ForgeRegistries.ITEMS.getValue(setRL("infused_lava_chestplate")));
         }
     };
     public static final ItemGroup AP_ITEM_GROUP = new ItemGroup(ItemGroup.getGroupCountSafe(), setName("items")) {
@@ -68,7 +88,33 @@ public class ArmorPlus {
         MinecraftForge.EVENT_BUS.register(this);
         BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        ENCHANTMENTS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        TILE_ENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
+        ENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
+        EFFECTS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientInit);
     }
+
+    @OnlyIn(Dist.CLIENT)
+    private void clientInit(FMLClientSetupEvent fml) {
+        registerRenderingHandler(CoalArrowEntity.class, "coal");
+        registerRenderingHandler(LapisArrowEntity.class, "lapis");
+        registerRenderingHandler(RedstoneArrowEntity.class, "redstone");
+        registerRenderingHandler(EmeraldArrowEntity.class, "emerald");
+        registerRenderingHandler(ObsidianArrowEntity.class, "obsidian");
+        registerRenderingHandler(InfusedLavaArrowEntity.class, "lava");
+        registerRenderingHandler(GuardianArrowEntity.class, "guardian");
+        registerRenderingHandler(SuperStarArrowEntity.class, "super_star");
+        registerRenderingHandler(EnderDragonArrowEntity.class, "ender_dragon");
+        ClientRegistry.bindTileEntitySpecialRenderer(SwordDisplayTile.class, new TESRSwordDisplay());
+    }
+
+    private static void registerRenderingHandler(Class<? extends APArrowEntity> entityClass, String name) {
+        RenderingRegistry.registerEntityRenderingHandler(entityClass, rm -> new APArrowRenderer<>(rm, name));
+    }
+
+    //      ClientRegistry.bindTileEntitySpecialRenderer(TileTrophy.class, new TESRTrophy());
+    //  ClientRegistry.bindTileEntitySpecialRenderer(TilePortal.class, new TESRPortal());
 
     public static ArmorPlus getInstance() {
         return instance;
