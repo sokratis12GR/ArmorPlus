4083999e6b0ff9d4e05c6c0b17c1bcb316ba9c1c
Sokratis Fotkatzikis <sokratis12GR@gmail.com>
2021-01-09 16:22:51 +0200

    ArmorPlus now contains all the new registry methods and config init.

--- src/main/java/com/sofodev/armorplus/ArmorPlus.java
+++ src/main/java/com/sofodev/armorplus/ArmorPlus.java
@@ -1,14 +1,20 @@
 package com.sofodev.armorplus;
 
+import com.sofodev.armorplus.config.APConfig;
 import com.sofodev.armorplus.events.WorldGenEvents;
-import com.sofodev.armorplus.registry.*;
+import com.sofodev.armorplus.registry.APItems;
+import com.sofodev.armorplus.registry.ModAttributes;
+import com.sofodev.armorplus.registry.ModBlocks;
+import com.sofodev.armorplus.registry.ModConfiguredFeatures;
 import com.sofodev.armorplus.registry.blocks.castle.BrickColor;
 import com.sofodev.armorplus.registry.entities.arrows.APArrowEntity;
 import com.sofodev.armorplus.registry.entities.arrows.APArrowRenderer;
 import com.sofodev.armorplus.registry.entities.bosses.SkeletalKingRenderer;
 import com.sofodev.armorplus.registry.entities.bosses.WitherlingRenderer;
 import com.sofodev.armorplus.registry.items.armors.APArmorMaterial;
-import com.sofodev.armorplus.registry.items.tools.properties.APToolProperties;
+import com.sofodev.armorplus.registry.items.tools.properties.mace.APMaceMaterial;
+import com.sofodev.armorplus.registry.items.tools.properties.tool.APToolProperties;
+import com.sofodev.armorplus.utils.GlobalVars;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
@@ -20,30 +26,41 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Effect;
 import net.minecraft.tileentity.TileEntityType;
-import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLPaths;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import software.bernie.geckolib3.GeckoLib;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+import static com.sofodev.armorplus.registry.APItems.registerAPItems;
 import static com.sofodev.armorplus.registry.ModBlocks.registerModBlocks;
 import static com.sofodev.armorplus.registry.ModEnchantments.registerEnchantments;
 import static com.sofodev.armorplus.registry.ModEntities.*;
+import static com.sofodev.armorplus.registry.ModFeatures.FEATURES;
+import static com.sofodev.armorplus.registry.ModItems.BOWS;
 import static com.sofodev.armorplus.registry.ModItems.registerModItems;
 import static com.sofodev.armorplus.utils.Utils.getAPItem;
 import static com.sofodev.armorplus.utils.Utils.setName;
+import static java.util.Arrays.asList;
+import static net.minecraft.item.ItemModelsProperties.registerProperty;
 import static net.minecraftforge.api.distmarker.Dist.CLIENT;
 import static net.minecraftforge.fml.client.registry.RenderingRegistry.registerEntityRenderingHandler;
 import static net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.MOD;
+import static net.minecraftforge.fml.config.ModConfig.Type.SERVER;
 import static net.minecraftforge.fml.loading.FMLEnvironment.dist;
 
 @Mod("armorplus")
@@ -61,7 +78,6 @@ public class ArmorPlus {
     public static final DeferredRegister<TileEntityType<?>> TILE_ENTITIES = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, MODID);
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITIES, MODID);
     public static final DeferredRegister<Effect> EFFECTS = DeferredRegister.create(ForgeRegistries.POTIONS, MODID);
-    public static final DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(ForgeRegistries.FEATURES, MODID);
     public static final DeferredRegister<Attribute> ATTRIBUTES = DeferredRegister.create(ForgeRegistries.ATTRIBUTES, MODID);
 
     /**
@@ -70,6 +86,7 @@ public class ArmorPlus {
     public static final int AP_ARMOR_MATERIAL_LENGTH = APArmorMaterial.values().length;
     public static final int AP_TOOL_MATERIAL_LENGTH = APToolProperties.values().length;
     public static final int AP_STONE_BRICKS_LENGTH = BrickColor.values().length;
+    public static final int AP_MACE_MAT_LENGTH = APMaceMaterial.values().length;
 
     public static final ItemGroup AP_GROUP = new ItemGroup(ItemGroup.getGroupCountSafe(), setName("armors")) {
         @Override
@@ -98,10 +115,14 @@ public class ArmorPlus {
 
     public ArmorPlus() {
         instance = this;
+        GeckoLib.initialize();
         MinecraftForge.EVENT_BUS.register(this);
         IEventBus forgeBus = MinecraftForge.EVENT_BUS;
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModLoadingContext modLoadingCTX = ModLoadingContext.get();
 
+        modLoadingCTX.registerConfig(SERVER, APConfig.SERVER_SPEC, "ap_config.toml");
+        APConfig.loadConfig(APConfig.SERVER_SPEC, FMLPaths.CONFIGDIR.get().resolve("ap_config.toml").toString());
         BLOCKS.register(modEventBus);
         ITEMS.register(modEventBus);
         ENCHANTMENTS.register(modEventBus);
@@ -110,6 +131,7 @@ public class ArmorPlus {
         EFFECTS.register(modEventBus);
         FEATURES.register(modEventBus);
         ATTRIBUTES.register(modEventBus);
+
         modEventBus.addListener(this::onCommonSetup);
         forgeBus.addListener(EventPriority.HIGH, WorldGenEvents::onBiomeLoad);
         if (dist == CLIENT) {
@@ -120,14 +142,15 @@ public class ArmorPlus {
     public void onCommonSetup(FMLCommonSetupEvent event) {
         registerModBlocks();
         registerModItems();
+        registerAPItems();
         registerEnchantments();
         registerEntities();
         event.enqueueWork(this::afterSetup);
     }
 
     private void afterSetup() {
+        GlobalVars.registerAfterEverything();
         ModAttributes.registerAttributes();
-        ModFeatures.registerFeatures();
         ModConfiguredFeatures.registerConfiguredFeatures();
     }
 
@@ -143,16 +166,33 @@ public class ArmorPlus {
         registerRenderingHandler(ENDER_DRAGON_ARROW.get(), "ender_dragon");
         registerEntityRenderingHandler(SKELETAL_KING.get(), SkeletalKingRenderer::new);
         registerEntityRenderingHandler(WITHERLING.get(), WitherlingRenderer::new);
-        this.setRenderLayer(ModBlocks.LAVA_INFUSER);
+        this.setRenderLayer(Collections.singletonList(ModBlocks.LAVA_INFUSER));
+        registerBowOverrides();
     }
 
-    private void setRenderLayer(RegistryObject<Block>... blocks) {
-        Arrays.stream(blocks).forEach(block -> RenderTypeLookup.setRenderLayer(block.get(), RenderType.getCutout()));
+    private void setRenderLayer(List<RegistryObject<Block>> blocks) {
+        blocks.forEach(block -> RenderTypeLookup.setRenderLayer(block.get(), RenderType.getCutout()));
     }
 
     @OnlyIn(CLIENT)
     private static void registerRenderingHandler(EntityType<? extends APArrowEntity> entityClass, String name) {
         registerEntityRenderingHandler(entityClass, rm -> new APArrowRenderer<>(rm, name));
+    }
+
+    @OnlyIn(CLIENT)
+    private static void registerBowOverrides() {
+        Arrays.stream(BOWS).forEach(bow -> {
+            registerProperty(bow.get(), new ResourceLocation("pull"), (stack, world, player) -> {
+                if (player == null) {
+                    return 0.0F;
+                } else {
+                    return player.getActiveItemStack() != stack ? 0.0F : (float) (stack.getUseDuration() - player.getItemInUseCount()) / 20.0F;
+                }
+            });
+            registerProperty(bow.get(), new ResourceLocation("pulling"), (stack, world, player) -> {
+                return player != null && player.isHandActive() && player.getActiveItemStack() == stack ? 1.0F : 0.0F;
+            });
+        });
     }
 
     //      ClientRegistry.bindTileEntitySpecialRenderer(TileTrophy.class, new TESRTrophy());
